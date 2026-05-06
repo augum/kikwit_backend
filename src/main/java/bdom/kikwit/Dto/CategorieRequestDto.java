066ec2e6d4cbee465b4d49180e53bf116016b5e8
Augustin <augumakuma@gmail.com>
2026-05-06 16:36:55 +0100

    Ajout de DTO pour categorie

---
+++ src/main/java/bdom/kikwit/Dto/CategorieRequestDto.java
@@ -0,0 +1,23 @@
+package bdom.kikwit.Dto;
+
+import bdom.kikwit.Entities.Etablissement;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+@JsonRootName(value="categorie")
+public class CategorieRequestDto {
+
+    private Long id;
+    private String libelle;
+    @OneToMany(mappedBy = "categorie")
+    private List<Etablissement> etablissementList;
+}
---
+++ src/main/java/bdom/kikwit/Dto/CategorieResponseDto.java
@@ -0,0 +1,19 @@
+package bdom.kikwit.Dto;
+
+import bdom.kikwit.Entities.Etablissement;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+@JsonRootName(value="categorie")
+public class CategorieResponseDto {
+    private Long id;
+    private String libelle;
+    @OneToMany(mappedBy = "categorie")
+    private List<Etablissement> etablissementList;
+}
