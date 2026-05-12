1b8ffa19ecb181e99e86b4b21811dc585779d70a
Augustin <augumakuma@gmail.com>
2026-05-12 13:08:23 +0100

    Correction erreur et premier demarrage test de application

--- src/main/java/bdom/kikwit/Entities/IndicateurTechnique.java
+++ src/main/java/bdom/kikwit/Entities/IndicateurTechnique.java
@@ -1,6 +1,9 @@
 package bdom.kikwit.Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +13,7 @@ import java.util.Date;
 
 @Entity @Data @NoArgsConstructor @AllArgsConstructor @ToString
 public class IndicateurTechnique {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date periode;
     private Long id_ess;
